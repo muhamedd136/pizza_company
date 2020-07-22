3421292a4d366305eb103a71faaf55ce151074b7
Muhamed Delihasanovic <48180244+muhamedd136@users.noreply.github.com>
2020-07-22 15:24:34 +0200

    add user, orders, menu models and controllers, functions for login, register, get menu, get orders, create order (#2)

--- server/database/migrations/2014_10_12_000000_create_users_table.php
+++ server/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users_test', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('full_name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
         });
     }
 
