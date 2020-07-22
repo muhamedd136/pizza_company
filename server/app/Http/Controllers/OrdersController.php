d0b8637738a41b185fae934c97b38428599fa065
“Muhamed <muhamed.delihasanovic@stu.ibu.edu.ba>
2020-07-22 18:13:05 +0200

    implement axios calls to backend

--- server/app/Http/Controllers/OrdersController.php
+++ server/app/Http/Controllers/OrdersController.php
@@ -9,20 +9,20 @@ use Illuminate\Support\Facades\DB;
 
 class OrdersController extends Controller
 {
-    public function getOrders(Request $request) {
+    public function getOrders(Request $request, $userId) {
 
-        $userId = $request['userId'];
+        // $userId = $request['userId'];
         
         $orders = DB::table('orders')->where("user_id", $userId)->get();
 
-        // return response()->json(compact('orders'));
-        return $orders;
+        return response()->json(compact('orders'));
+        // return $orders;
     }
 
     public function createOrder(Request $request) {
 
         DB::table('orders')->insert([
-            'user_id' => $request['userId'], 'details' => $request['details'], 'note' => $request['note'], 'created' => $request['created']
+            'user_id' => $request['userId'], 'details' => $request['details'], 'created' => $request['created'], 'total' => $request['total']
         ]);
 
         return response()->json(['order_successful'], 200);
